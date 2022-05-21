99b99120cf68912d6fb325e924478a15b6779214
yanchiktg <svzwyana075@mail.ru>
2022-05-21 22:31:53 +0300

    Add Fields

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,24 @@
 package com.example.yo_calc;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String OldNumber;
+    String Operator = "";
+    Boolean Cheker = true;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editText = findViewById(R.id.Result);
     }
+
 }
